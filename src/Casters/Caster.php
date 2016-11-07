81e9b0748a285fec52cec2d717f5c2cce8c239cf
Pavel Koch <pavel.koch@wrongware.cz>
2016-11-07 17:56:59 +0000

    Redundant comments

--- src/Casters/Caster.php
+++ src/Casters/Caster.php
@@ -62,32 +62,22 @@ abstract class Caster implements CasterContract
      */
     private function resolveCast($old, $desired, Model $model, &$transformed)
     {
-        // If there was a closure provided as a value of the array
-        // run that closure and return it as a result of casting.
         if ($desired instanceof Closure) {
             return $transformed[$old] = call_user_func($desired, $model);
         }
 
-        // If the value was prefixed with the function sign, which
-        // is '@' by default, call desired method on this class.
         if (is_string($desired) && strpos($desired, $this->functionSign) !== false) {
             return $transformed[$old] = call_user_func([$this, substr($desired, 1)], $model);
         }
 
-        // If it was prefixed with the query sign, which
-        // is '!' by default, make use of the CastBuilder instance
-        // and resolve the cast there.
         if (is_string($desired) && strpos($desired, $this->querySign) !== false) {
             return $this->parse($old, substr($desired, 1), $model, $transformed);
         }
 
-        // If they specified simple key - value string pairs, simply
-        // rename the column, retaining the contents.
         if (is_string($old)) {
             return $transformed[$desired] = $model->$old;
         }
 
-        // Otherwise keep unchanged.
         return $transformed[$desired] = $model->$desired;
     }
 
